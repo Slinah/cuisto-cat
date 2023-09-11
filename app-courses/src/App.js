0010fc2b306a43255bdfea211a93b5c95c623e22
Fullpacki <113352776+Fullpacki@users.noreply.github.com>
2023-09-11 15:05:35 +0200

    reset

--- app-courses/src/App.js
+++ app-courses/src/App.js
@@ -1,13 +1,5 @@
-import Article from "./components/Article";
-import Navbar from "./components/Navbar";
-
 function App() {
-  return (
-    <div className="App border py-5 px-1 ">
-      <Navbar />
-      <Article />
-    </div>
-  );
+  return <div className="App border py-5 px-1 ">Ceci est une application</div>;
 }
 
 export default App;
