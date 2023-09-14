e8d71ed2181223d6c697f8eb52437e745c429f09
Fullpacki <113352776+Fullpacki@users.noreply.github.com>
2023-09-14 17:13:59 +0200

    Reset

--- app-courses/src/App.js
+++ app-courses/src/App.js
@@ -1,17 +1,7 @@
-import Categorie from "./components/Categorie";
-import Dishes from "./components/Dishes";
-import MenuMobile from "./components/MenuMobile";
-import Navbar from "./components/Navbar";
-import SearchBar from "./components/SearchBar";
-
 function App() {
   return (
     <div className="App flex flex-col gap-7 px-3 py-5 pb-32 max-w-[375px] relative">
-      <Navbar />
-      <SearchBar />
-      <Categorie />
-      <Dishes />
-      <MenuMobile />
+      1234
     </div>
   );
 }
