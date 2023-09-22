c2a4359f50114d9d7b658292dc91ec343cdda09c
Fullpacki <113352776+Fullpacki@users.noreply.github.com>
2023-09-22 17:03:06 +0200

    Remise à zéro
    Je recommence tout après, j'ai besoin de comprendre l'ajout au panier, puis je vais remettre tailwindcss

--- app-courses/src/App.js
+++ app-courses/src/App.js
@@ -1,19 +1,5 @@
-import Categories from "./components/Categories";
-import MobileMenu from "./components/MobileMenu";
-import Navbar from "./components/Navbar";
-import NewDishes from "./components/NewDishes";
-import InputFinder from "./components/inputFinder";
-import "./styles/app.css";
-
 function App() {
-  return (
-    <div className="app">
-      <Navbar />
-      <Categories />
-      <NewDishes />
-      <MobileMenu />
-    </div>
-  );
+  return <div className="app">1234</div>;
 }
 
 export default App;
